49c3c7eac6f1937ad47348a6b29a982c7bad1cae
Sidoine De Wispelaere <sidoine@sidoine.net>
2017-10-17 21:13:42 +0200

    It's not an embedable library

--- index.ts
+++ index.ts
@@ -1,30 +1,20 @@
 import { Constructor, Library} from "@wowts/tslib";
 import { LuaObj } from "@wowts/lua";
 
-export interface AceConfigDialog {
-    SetDefaultSize(appName:string, width: number, height:number):void;
-    Open(appName: string):void;
-    Close(appName: string):void;
-    AddToBlizOptions(appName: string, name?: string, addon?:string):void;
-    OpenFrames:LuaObj<any>;
+export class AceConfigDialog {
+    SetDefaultSize(appName:string, width: number, height:number):void{
+    }
+    Open(appName: string):void {
+        this.OpenFrames[appName] = {};
+    }
+    Close(appName: string):void {
+        delete this.OpenFrames[appName];
+    }
+    AddToBlizOptions(appName: string, name?: string, addon?:string):void {
+    }
+    OpenFrames:LuaObj<any> = {}
 }
 
-const lib: Library<AceConfigDialog> = {
-    Embed<T extends Constructor<{}>>(Base: T): Constructor<AceConfigDialog> & T {
-        return class extends Base {
-            SetDefaultSize(appName:string, width: number, height:number):void{
-            }
-            Open(appName: string):void {
-                this.OpenFrames[appName] = {};
-            }
-            Close(appName: string):void {
-                delete this.OpenFrames[appName];
-            }
-            AddToBlizOptions(appName: string, name?: string, addon?:string):void {
-            }
-            OpenFrames:LuaObj<any> = {}
-        };
-    },
-};
+const lib = new AceConfigDialog();
 export default lib;
 
