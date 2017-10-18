d28c139f5a7a5f99544ea73c79d91eee585f438f
Sidoine De Wispelaere <sidoine@sidoine.net>
2017-10-18 13:52:44 +0200

    Compiled files

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,10 @@
-import { Library } from "@wowts/tslib";
 import { LuaObj } from "@wowts/lua";
-export interface AceConfigDialog {
+export declare class AceConfigDialog {
     SetDefaultSize(appName: string, width: number, height: number): void;
     Open(appName: string): void;
     Close(appName: string): void;
     AddToBlizOptions(appName: string, name?: string, addon?: string): void;
     OpenFrames: LuaObj<any>;
 }
-declare const lib: Library<AceConfigDialog>;
+declare const lib: AceConfigDialog;
 export default lib;
--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const lib = {
-    Embed(Base) {
-        return class extends Base {
-            constructor() {
-                super(...arguments);
-                this.OpenFrames = {};
-            }
-            SetDefaultSize(appName, width, height) {
-            }
-            Open(appName) {
-                this.OpenFrames[appName] = {};
-            }
-            Close(appName) {
-                delete this.OpenFrames[appName];
-            }
-            AddToBlizOptions(appName, name, addon) {
-            }
-        };
-    },
-};
+class AceConfigDialog {
+    constructor() {
+        this.OpenFrames = {};
+    }
+    SetDefaultSize(appName, width, height) {
+    }
+    Open(appName) {
+        this.OpenFrames[appName] = {};
+    }
+    Close(appName) {
+        delete this.OpenFrames[appName];
+    }
+    AddToBlizOptions(appName, name, addon) {
+    }
+}
+exports.AceConfigDialog = AceConfigDialog;
+const lib = new AceConfigDialog();
 exports.default = lib;
